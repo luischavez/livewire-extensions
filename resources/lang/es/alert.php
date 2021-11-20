e58b8c77f7247fe35a092a3fa8cd2e6237ff0076
Luis Chávez <frost_leviathan@hotmail.com>
2021-11-19 20:34:01 -0700

    Draft

---
+++ resources/lang/es/alert.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'cancel'    => 'Cancelar',
+    'confirm'   => 'Confirmar',
+
+    'throw' => [
+        'title'     => 'Error',
+        'message'   => 'Contacta a soporte',
+    ],
+];
---
+++ resources/lang/es/auth.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'login_header'      => 'Ingresar',
+    'register_header'   => 'Registrar',
+
+    'name'              => 'Nombre',
+    'email'             => 'Correo',
+    'password'          => 'Contraseña',
+    'confirm_password'  => 'Confirmar Contraseña',
+    'remember'          => 'Recordar?',
+    
+    'cancel'    => 'Cancelar',
+    'login'     => 'Ingresar',
+    'register'  => 'Registrar',
+
+    'new_user'              => 'Nuevo usuario?',
+    'create_account'        => 'Crear cuenta',
+    'already_registered'    => 'Ya estas registrado?',
+
+    'pages' => [
+        'login' => [
+            'title_success'     => 'Ingresaste correctamente!',
+            'message_success'   => 'Bienvenido de nuevo!',
+
+            'title_error'   => 'Error',
+            'message_error' => 'Verifica tus credenciales',
+        ],
+        'register' => [
+            'title_success'     => 'Registrado correctamente!',
+            'message_success'   => 'Bienvenido!',
+
+            'title_error'   => 'Error',
+            'message_error' => 'Verifica tu informacion o contacta a soporte',
+        ],
+    ],
+
+    'attributes' => [
+        'name'                  => 'Nombre',
+        'email'                 => 'Correo',
+        'password'              => 'Contraseña',
+        'password_confirmation' => 'Confirmar Contraseña',
+        'remember'              => 'Recordar',
+    ],
+];
